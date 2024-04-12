11f4173e6c946ca88ba827999bc0034fcaa75749
alirezaseif20 <alirezaariye@gmail.com>
2024-04-12 16:00:51 +0330

    feat: add app colors

---
+++ lib/constants/colors.dart
@@ -0,0 +1,7 @@
+
+import 'package:flutter/material.dart';
+
+final Color textGreen = Color(0xFF38b387);
+final Color textGrey = Color(0xFF93a59e);
+final Color backgroundGrey = Color(0xFF2e3951);
+final Color backgroundGreyDark = Color(0xFF212b41);

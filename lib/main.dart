f23d713d82ea0ea657d4240d1abfde8431ab5b3c
alirezaseif20 <alirezaariye@gmail.com>
2024-04-12 16:43:45 +0330

    feat: add keybord

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:calculator/constants/colors.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,9 +11,114 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+      debugShowCheckedModeBanner: false,
+      home: Application(),
+    );
+  }
+}
+
+class Application extends StatelessWidget {
+  const Application({super.key});
+
+  Widget getRow(String text1, String text2, String text3, String text4) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: [
+        TextButton(
+          onPressed: () {},
+          child: Padding(
+            padding: const EdgeInsets.all(3.0),
+            child: Text(
+              text1,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: textGrey,
+                fontSize: 26,
+              ),
+            ),
+          ),
+        ),
+        TextButton(
+          child: Padding(
+            padding: const EdgeInsets.all(3.0),
+            child: Text(
+              text2,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: textGrey,
+                fontSize: 26,
+              ),
+            ),
+          ),
+          onPressed: () {},
+        ),
+        TextButton(
+          child: Padding(
+            padding: const EdgeInsets.all(3.0),
+            child: Text(
+              text3,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: textGrey,
+                fontSize: 26,
+              ),
+            ),
+          ),
+          onPressed: () {},
+        ),
+        TextButton(
+          child: Padding(
+            padding: const EdgeInsets.all(3.0),
+            child: Text(
+              text4,
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: textGrey,
+                fontSize: 26,
+              ),
+            ),
+          ),
+          onPressed: () {},
+        ),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              flex: 3,
+              child: Container(
+                color: backgroundGreyDark,
+              ),
+            ),
+            Expanded(
+              flex: 7,
+              child: Container(
+                color: backgroundGrey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    getRow(
+                      'ac',
+                      'ce',
+                      '%',
+                      '/',
+                    ),
+                    getRow('7', '8', '9', '*'),
+                    getRow('4', '5', '6', '-'),
+                    getRow('1', '2', '3', '+'),
+                    getRow('00', '0', '.', '='),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
